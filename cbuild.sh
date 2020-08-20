62f353e0740300c5cb610f677607c4099b217094
Radwan Suufi <rd.90@hotmail.com>
2020-08-20 21:37:59 +0100

    Fix build

--- cbuild.sh
+++ cbuild.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
+# Clean build directory
 if [ ! -d ./build ];then
   mkdir build
 else
-  if [ -d bin];then
-    rm -rf bin
-   fi
   rm -rf build
   mkdir build
 fi
 
-cd build;
+# Remove build artifacts.
+if [ -d ./bin];then
+  rm -rf bin
+fi
+
+# Build project
+cd build
 cmake ..
-jmake install
+make install
