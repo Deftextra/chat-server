c618187327aa233f8700cf056edfe4e1587b3507
Radwan Suufi <rd.90@hotmail.com>
2020-08-20 20:58:44 +0100

    Pull request test for CI build

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -5,5 +5,7 @@ int main()
 {
   std::cout << "Testing CI build" << std::endl;
   std::cout << "Testting CI build seconde time" << std::endl;
+  std::cout << "Testting CI build third time" << std::endl;
+
   return 0;
 }
