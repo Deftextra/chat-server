ea1fc50127e700d8969f2c572eb0bce25b77def5
Radwan Suufi <rd.90@hotmail.com>
2020-09-14 16:10:28 +0000

    Remove include directory
    When Running a clean build remove include directory.

--- cbuild.sh
+++ cbuild.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# This file will run a clean build.
+
 # Clean build directory
 if [ ! -d ./build ];then
   mkdir build
@@ -11,6 +13,10 @@ fi
 # Remove build artifacts.
 if [ -d ./bin ];then
   rm -rf bin
+fi
+
+if [ -d ./include ];then
+  rm -rf include
 fi
 
 # Build project
