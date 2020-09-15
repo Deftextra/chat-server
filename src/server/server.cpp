6778ce048eb6f2a1eb8e72b7f4190dcf05642662
Radwan Suufi <rd.90@hotmail.com>
2020-09-15 10:50:58 +0100

    Create server class.

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -1,2 +1,27 @@
 #include "server.hpp"
+#include <functional>
 
+chat::server::server(io::io_context& io_context, std::uint16_t port)
+    : acceptor{io_context, tcp::endpoint{tcp::v4(), port}},
+      io_context{io_context} {}
+
+void chat::server::async_accept() {
+  acceptor.async_accept([this](error_code error, tcp::socket socket) {
+    if (!error) {
+      auto client = std::make_shared<session>(std::move(socket));
+
+      chat_room.join(client);
+
+      client->start(
+          std::bind(&room::broadcast, &chat_room, std::placeholders::_1),
+          [&, client] {
+            chat_room.leave(client);
+            // TODO: capture the case when session has no name. This should
+            // never occur.
+            chat_room.broadcast(client->compose_name() + "has left the chat");
+          });
+
+      async_accept();
+    }
+  });
+}
--- src/server/server.hpp
+++ src/server/server.hpp
@@ -2,17 +2,22 @@
 #define SERVER_HPP
 
 #include "asp.hpp"
+#include "room/room.hpp"
+#include "session/session.hpp"
 
 namespace chat {
 class server {
  public:
   server(io::io_context& io_context, std::uint16_t port);
 
+  void async_accept();
 
  private:
   io::io_context& io_context;
   tcp::acceptor acceptor;
+  room chat_room;
 };
+
 }  // namespace chat
 
 #endif /* SERVER_HPP */
