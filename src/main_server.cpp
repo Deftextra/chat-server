4034f725a3286b7e4bd3cfbe9e1f238ddaf60a43
Radwan Suufi <rd.90@hotmail.com>
2020-09-15 10:51:33 +0100

    Add server in main for testing.

--- src/main_server.cpp
+++ src/main_server.cpp
@@ -1,3 +1,9 @@
 #include <iostream>
+#include <server.hpp>
 
-int main(int argc, char* argv[]) { std::cout << "Hello world" << std::endl; }
+int main(int argc, char* argv[]) {
+  io::io_context io_context;
+  chat::server srv(io_context, 15001);
+  srv.async_accept();
+  io_context.run();
+}
